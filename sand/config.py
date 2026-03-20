96b036cb5ed5dbf1772eb76684869b8018060364
Veddy1674 <veddytasses@gmail.com>
2026-03-20 22:50:52 +0100

    Updated, tested and de-overparameterized

--- sand/config.py
+++ sand/config.py
@@ -2,18 +2,24 @@ from NCA import NCA
 import torch
 
 # train params
-STEPS = 1_000
+STEPS = 1_500
 BATCH_SIZE = 12 # increases total number of samples, along with VRAM usage
-LOG_SEGMENTS = 10
+LOG_SEGMENTS = 100
 # LOAD_MODEL = "sand/sand.pt" # what train.py loads (comment out to train from scratch)
-FILE_NAME = "sand/sand2.pt" # result model file name (post training)
+FILE_NAME = "sand/sand.pt" # result model file name (post training)
 DATA_GLOB = "sand/dev/data/sand_*.npz"
-MICROSTEPS = 12
+MICROSTEPS = 4
+TRAIN_STEPS = 1
+POOL_LENGTH = None
+LOAD_QUICK = True # wheter to load all the datasets to RAM or use lazy loading
+LOAD_INSTANT = True # wheter to load everything to VRAM right away (for small datasets where CPU is the bottleneck)
+EXTRA_MAPS = {}
+LOSS_GRAPH = "sand/loss_graph.png" # can be None
 
 # visualizer params
-FIRST_DATA_FILE = "sand/dev/data/sand_0_26116.npz"
-GRID_SIZE = (128, 128) # H, W
-STARTING_IMAGE = "sand/start_img2.png" # repeated for each input_length for simplicity
+FIRST_DATA_FILE = "sand/dev/data/sand_0_43962.npz"
+GRID_SIZE = (96, 96) # H, W
+STARTING_IMAGE = "sand/start_img.png" # repeated for each input_length for simplicity
 BIT_DEPTH_LEVELS = 256
 
 base_size = 900
@@ -50,18 +56,25 @@ KEY_MAP = {
 DEFAULT_KEY = 0 # noop
 FPS = 60
 
-model = NCA(actions=4, vis_channels=len(COLOR_MAP), hid_channels=16, input_length=2, padding_mode='zeros', device='cuda')
-optimizer = torch.optim.Adam(model.parameters(), lr=6e-4)
+TEMPERATURE = 1.0
+TOP_P = 0.99
 
-# the way it worked the best for me was, training on weights focused on floor, rect, background and sand weighted 0
-# and then fine-tuned to 0.1, 0.5, 6.0, 4.0 or similar, to focus on the sand physics (while making sure it doesn't forget rect and floor)
+LOAD_OPTIMIZER = True
 
+# surprisingly, hidden channels=0, hidden_neurons=64 and input_lenght=1 work as well
+# as a overparameterized model with 16 hidden channels, 128 hidden neurons and 2 input length.
+model = NCA(actions=4, vis_channels=len(COLOR_MAP), hid_channels=0, input_length=1, hidden_neurons=64, padding_mode='zeros', device='cuda')
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9999) # with 1500 steps, from 1e-3 to 8.6e-4
+
+# fine-tuning with sand weight to zero and then back up works fine, but I found this combination to work as well and without fine-tuning
 # floor, rect, sand, background
-weights = torch.tensor([0.1, 1.0, 6.0, 4.0], device=model.device)
+weights = torch.tensor([0.1, 0.5, 1.7, 1.5], device=model.device)
 loss_func = torch.nn.CrossEntropyLoss(weight=weights)
 
 # customizable function which defines the loss
-def loss_calc(model_pred: torch.Tensor, actions: torch.Tensor, states: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+def loss_calc(model_pred: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
     pred_visible = model_pred[:, :model.vis_channels] # loss on visible channels
     return loss_func(pred_visible, targets.argmax(dim=1))
 
@@ -77,11 +90,11 @@ def post_processing(img: np.ndarray) -> np.ndarray:
     sand_mask = np.all(result == sand_color, axis=2)
     
     if np.any(sand_mask):
-        h, w = img.shape[:2]
-        
+        w = img.shape[1] # h, w
+
         # gradient in BGR
-        color_start = np.array([71, 169, 191])
-        color_end = np.array([38, 38, 255])
+        color_start = np.array([71, 169, 191]) # dark yellow
+        color_end = np.array([38, 38, 255]) # bright red
         
         # gradient on column
         y_indices, x_indices = np.where(sand_mask)
